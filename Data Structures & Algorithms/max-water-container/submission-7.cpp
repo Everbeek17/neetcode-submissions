8f013b61836bd196b758bb61853da44b7e38abd3
Erkin Verbeek <33463698+Everbeek17@users.noreply.github.com>
2026-05-08 17:41:13 -0400

    Add: max-water-container - submission-7

---
+++ Data Structures & Algorithms/max-water-container/submission-7.cpp
@@ -0,0 +1,35 @@
+#include <algorithm> // for std::max()
+
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int n = heights.size();
+
+        int left_idx = 0;
+        int right_idx = n - 1;
+        int left_height = heights[left_idx];
+        int right_height = heights[right_idx];
+
+        int curr_vol = std::min(left_height, right_height) * (right_idx - left_idx);
+        int potential_vol;
+        
+        while (left_idx < right_idx)
+        {            
+            if (left_height > right_height)
+            {
+                right_height = heights[--right_idx];
+            }
+            else
+            {
+                left_height = heights[++left_idx];
+            }
+            potential_vol = std::min(left_height, right_height) * (right_idx - left_idx);
+            if (potential_vol > curr_vol)
+            {
+                curr_vol = potential_vol;
+            }
+        }
+        
+        return curr_vol;
+    }
+};
